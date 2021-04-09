6a10229be1b91588a2bac32889488a850bc9ce42
GraziBerti <bertti.gl@gmail.com>
2021-04-09 16:14:28 -0300

    setting up the user interface

---
+++ goodNewsApp/goodNewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goodNewsApp
+//
+//  Created by Grazi Berti on 09/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
