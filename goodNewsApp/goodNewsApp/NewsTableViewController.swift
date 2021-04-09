eff390dec35127211de7f06139a2e1ce07907144
GraziBerti <bertti.gl@gmail.com>
2021-04-09 16:50:36 -0300

    implementing models

--- goodNewsApp/goodNewsApp/NewsTableViewController.swift
+++ goodNewsApp/goodNewsApp/NewsTableViewController.swift
@@ -6,16 +6,46 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsTableViewController: UITableViewController {
-
+    
+    let disposeBag = DisposeBag()
+    
+    private var articles = [Article]()
    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Good News"
-        
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        populateNews()
     }
+    
+    private func populateNews() {
+        
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=br&apiKey=0cf790498275413a9247f8b94b3843fd")!
+        
+        Observable.just(url)
+            .flatMap { url -> Observable<Data> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+            }.map { data -> [Article]? in
+                return try? JSONDecoder().decode(ArticleList.self, from: data).articles
+            }.subscribe(onNext: { [weak self] articles in
+                
+                if let articles = articles {
+                    self?.articles = articles
+                    DispatchQueue.main.async {
+                        self?.tableView.reloadData()
+                    }
+                }
+            }).disposed(by: disposeBag)
+    }
+
+    
+
 
     // MARK: - Table view data source
 
